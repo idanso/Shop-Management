6520012ac16c7e5a6f55294b6fc4ba091486985b
Alon Ronder <alonr1110@gmail.com>
2021-02-18 14:16:17 +0200

    update

--- shop_managment_project/src/View/MainWindow.java
+++ shop_managment_project/src/View/MainWindow.java
@@ -6,9 +6,12 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.FontPosture;
@@ -31,15 +34,15 @@ public class MainWindow extends Application{
 		window.setTitle("Main window");
 		Button addProduct = new Button("Add product");
 		addProduct.setOnAction(e -> window.setScene(addProductScene));
-		Button sorting = new Button ("Select type of sorting");
-		Button showProduct = new Button ("Show all the product");
+		Button showProduct = new Button ("Show all the products");
+		Button sendNotification = new Button ("Send notification");
 		Button exit = new Button ("EXIT");
 		exit.setOnAction(e ->{
 			System.out.println("Good bye!");
 			System.exit(0);
 		});
 		VBox vbox = new VBox();
-		vbox.getChildren().addAll(addProduct, sorting, showProduct, exit);
+		vbox.getChildren().addAll(addProduct, showProduct, sendNotification, exit);
 		vbox.setAlignment(Pos.CENTER);
 		vbox.setPadding(new Insets(20,80,20,80));
 		vbox.setSpacing(10);
@@ -48,32 +51,57 @@ public class MainWindow extends Application{
 		
 		
 		//addProduct scene
-		HBox hbox1 = new HBox(10);
-		HBox hbox2 = new HBox(10);
-		HBox hbox3 = new HBox(10);
-		VBox vboxAddProduct = new VBox();
-		Button btn = new Button("Submmit");
-		btn.setOnAction(e -> {
-//			System.out.println(txt1);
+		GridPane grid = new GridPane();
+		HBox hbox1 = new HBox(8);
+		Alert alert = new Alert(AlertType.NONE);
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(5);
+		grid.setHgap(10);
+		Button submmitB = new Button("Submmit");
+		Button clearB = new Button ("Clear");
+		
+		Label head = new Label ("Add product:");
+		
+		TextField txt1 = new TextField();
+		txt1.setPromptText("Product Name");
+		
+		TextField txt2 = new TextField();
+		txt2.setPromptText("Price for the shop");
+		
+		TextField txt3 = new TextField();
+		txt3.setPromptText("Costumer price");
+		TextField txt4 = new TextField();
+		
+		txt4.setPromptText("Costumer Name");
+		TextField txt5 = new TextField();
+		
+		txt5.setPromptText("Phone number");
+		grid.add(head,0,0);
+		grid.add(txt1, 0, 1);
+		grid.add(txt2, 1, 1);
+		grid.add(txt3, 0, 2);
+		grid.add(txt4, 1, 2);
+		grid.add(txt5, 0, 3);
+		grid.add(hbox1, 0, 4);
+		
+		hbox1.getChildren().addAll(submmitB, clearB);
+		submmitB.setOnAction(e -> {
+				if(txt1.getText().isEmpty() || txt2.getText().isEmpty() ||
+						txt3.getText().isEmpty() || txt4.getText().isEmpty() || txt5.getText().isEmpty()) {
+					alert.setAlertType(AlertType.ERROR);
+					alert.setContentText("Fill all up before the dreadful idan will kill you!");
+					alert.show();
+				}
 		});
-		Text head = new Text ();
-		head.setText("Add product");
-		//head.setTextAlignment(Pos.TOP_CENTER);
-		//head.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
-		TextField txt1 = new TextField("Product Name");
-//		txt1.getOnAction(e ->{
-//			
-//		});
-		TextField txt2 = new TextField("Price for the shop");
-		hbox1.getChildren().addAll(txt1, txt2);
-		TextField txt3 = new TextField("Costumer price");
-		TextField txt4 = new TextField("Costumer Name");
-		hbox2.getChildren().addAll(txt3, txt4);
-		TextField txt5 = new TextField("Phone number");
-		hbox3.getChildren().addAll(txt5);
-		vboxAddProduct.setSpacing(20);
-		vboxAddProduct.getChildren().addAll(head, hbox1,hbox2,hbox3);
-		addProductScene = new Scene(vboxAddProduct, 500,500);
+		clearB.setOnAction(e -> {
+			txt1.clear();
+			txt2.clear();
+			txt3.clear();
+			txt4.clear();
+			txt5.clear();
+		});
+		grid.setAlignment(Pos.BASELINE_CENTER);
+		addProductScene = new Scene(grid, 500,300);
 		
 		
 		window.setScene(starter);
